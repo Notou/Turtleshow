511b7eed324ba6743142b642b28534da1a306c69
Benoit Bregeault <b.bregeault@posteo.net>
2021-10-18 15:14:10 +0200

    Extract constants from the class and fix indentation

--- scripts/control.py
+++ scripts/control.py
@@ -13,6 +13,78 @@ import sys, tty, termios
 import threading
 
 
+# These constants were borrowed from linux/input.h
+AXIS_NAMES = {
+    0x00 : 'x',
+    0x01 : 'y',
+    0x02 : 'z',
+    0x03 : 'rx',
+    0x04 : 'ry',
+    0x05 : 'rz',
+    0x06 : 'trottle',
+    0x07 : 'rudder',
+    0x08 : 'wheel',
+    0x09 : 'gas',
+    0x0a : 'brake',
+    0x10 : 'hat0x',
+    0x11 : 'hat0y',
+    0x12 : 'hat1x',
+    0x13 : 'hat1y',
+    0x14 : 'hat2x',
+    0x15 : 'hat2y',
+    0x16 : 'hat3x',
+    0x17 : 'hat3y',
+    0x18 : 'pressure',
+    0x19 : 'distance',
+    0x1a : 'tilt_x',
+    0x1b : 'tilt_y',
+    0x1c : 'tool_width',
+    0x20 : 'volume',
+    0x28 : 'misc',
+}
+
+BUTTON_NAMES = {
+    0x120 : 'trigger',
+    0x121 : 'thumb',
+    0x122 : 'thumb2',
+    0x123 : 'top',
+    0x124 : 'top2',
+    0x125 : 'pinkie',
+    0x126 : 'base',
+    0x127 : 'base2',
+    0x128 : 'base3',
+    0x129 : 'base4',
+    0x12a : 'base5',
+    0x12b : 'base6',
+    0x12f : 'dead',
+    0x130 : 'a',
+    0x131 : 'b',
+    0x132 : 'c',
+    0x133 : 'x',
+    0x134 : 'y',
+    0x135 : 'z',
+    0x136 : 'tl',
+    0x137 : 'tr',
+    0x138 : 'tl2',
+    0x139 : 'tr2',
+    0x13a : 'select',
+    0x13b : 'start',
+    0x13c : 'mode',
+    0x13d : 'thumbl',
+    0x13e : 'thumbr',
+
+    0x220 : 'dpad_up',
+    0x221 : 'dpad_down',
+    0x222 : 'dpad_left',
+    0x223 : 'dpad_right',
+
+    # XBox 360 controller uses these codes.
+    0x2c0 : 'dpad_left',
+    0x2c1 : 'dpad_right',
+    0x2c2 : 'dpad_up',
+    0x2c3 : 'dpad_down',
+}
+
 #Handle joystick inputs
 class stick():
     def __init__(self, calling):
@@ -28,78 +100,6 @@ class stick():
         # We'll store the states here.
         self.axis_states = {}
         self.button_states = {}
-
-        # These constants were borrowed from linux/input.h
-        self.axis_names = {
-        0x00 : 'x',
-        0x01 : 'y',
-        0x02 : 'z',
-        0x03 : 'rx',
-        0x04 : 'ry',
-        0x05 : 'rz',
-        0x06 : 'trottle',
-        0x07 : 'rudder',
-        0x08 : 'wheel',
-        0x09 : 'gas',
-        0x0a : 'brake',
-        0x10 : 'hat0x',
-        0x11 : 'hat0y',
-        0x12 : 'hat1x',
-        0x13 : 'hat1y',
-        0x14 : 'hat2x',
-        0x15 : 'hat2y',
-        0x16 : 'hat3x',
-        0x17 : 'hat3y',
-        0x18 : 'pressure',
-        0x19 : 'distance',
-        0x1a : 'tilt_x',
-        0x1b : 'tilt_y',
-        0x1c : 'tool_width',
-        0x20 : 'volume',
-        0x28 : 'misc',
-        }
-
-        self.button_names = {
-        0x120 : 'trigger',
-        0x121 : 'thumb',
-        0x122 : 'thumb2',
-        0x123 : 'top',
-        0x124 : 'top2',
-        0x125 : 'pinkie',
-        0x126 : 'base',
-        0x127 : 'base2',
-        0x128 : 'base3',
-        0x129 : 'base4',
-        0x12a : 'base5',
-        0x12b : 'base6',
-        0x12f : 'dead',
-        0x130 : 'a',
-        0x131 : 'b',
-        0x132 : 'c',
-        0x133 : 'x',
-        0x134 : 'y',
-        0x135 : 'z',
-        0x136 : 'tl',
-        0x137 : 'tr',
-        0x138 : 'tl2',
-        0x139 : 'tr2',
-        0x13a : 'select',
-        0x13b : 'start',
-        0x13c : 'mode',
-        0x13d : 'thumbl',
-        0x13e : 'thumbr',
-
-        0x220 : 'dpad_up',
-        0x221 : 'dpad_down',
-        0x222 : 'dpad_left',
-        0x223 : 'dpad_right',
-
-        # XBox 360 controller uses these codes.
-        0x2c0 : 'dpad_left',
-        0x2c1 : 'dpad_right',
-        0x2c2 : 'dpad_up',
-        0x2c3 : 'dpad_down',
-        }
 
         self.axis_map = []
         self.button_map = []
@@ -136,21 +136,21 @@ class stick():
         ioctl(self.jsdev, 0x80406a32, buf) # JSIOCGAXMAP
 
         for axis in buf[:num_axes]:
-            axis_name = self.axis_names.get(axis, 'unknown(0x%02x)' % axis)
+            axis_name = AXIS_NAMES.get(axis, 'unknown(0x%02x)' % axis)
             self.axis_map.append(axis_name)
             self.axis_states[axis_name] = 0.0
 
-            # Get the button map.
-            buf = array.array('H', [0] * 200)
-            ioctl(self.jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
+        # Get the button map.
+        buf = array.array('H', [0] * 200)
+        ioctl(self.jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
 
-            for btn in buf[:num_buttons]:
-                btn_name = self.button_names.get(btn, 'unknown(0x%03x)' % btn)
-                self.button_map.append(btn_name)
-                self.button_states[btn_name] = 0
+        for btn in buf[:num_buttons]:
+            btn_name = BUTTON_NAMES.get(btn, 'unknown(0x%03x)' % btn)
+            self.button_map.append(btn_name)
+            self.button_states[btn_name] = 0
 
-                print('%d axes found: %s' % (num_axes, ', '.join(self.axis_map)))
-                print('%d buttons found: %s' % (num_buttons, ', '.join(self.button_map)))
+        print('%d axes found: %s' % (num_axes, ', '.join(self.axis_map)))
+        print('%d buttons found: %s' % (num_buttons, ', '.join(self.button_map)))
 
     def updatePositionLoop(self):
         if not self.controller:
